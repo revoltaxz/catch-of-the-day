a7390171ddb0c3b1115e63f70bd1a7dea338690f
Guilherme Revolta <guilhermerevolta@gmail.com>
2017-10-02 10:34:59 -0300

    Create a Header component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
+import './css/style.css';
+import App from './components/App';
 
-import StorePicker from './components/StorePicker';
-
-
-render(<StorePicker/>,  document.querySelector('#root'));
+render(<App/>,  document.querySelector('#root'));
 
